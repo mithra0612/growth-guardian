9fd27b5d22020e254f47f09f6084bdafc52ec354
Madhumithra <mithramadhu005@gmail.com>
2025-03-15 11:49:15 +0530

    tools

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import RetirementPlanner from './CalculationTools/RetirementPlanner'
 import InvestmentVsDebtRepayment from './CalculationTools/InvestmentVsDebtRepayment'
+import BudgetPlanner from './CalculationTools/BudgetPlanner'
 function App() {
   return (
     <div>
       {/* <RetirementPlanner /> */}
       <InvestmentVsDebtRepayment/>
+      {/* <BudgetPlanner/> */}
     </div>
   )
 }
